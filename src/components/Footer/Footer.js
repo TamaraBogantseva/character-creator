b5a7f4d75a7463b83fa8bdb79d837c37ef01ee52
TamaraBogantseva <tamara.bogantseva@compose.us>
2023-03-15 13:34:02 +0100

    fix: footer link
    Signed-off-by: TamaraBogantseva <tamara.bogantseva@compose.us>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      All character assets taken from{' '}
-      <a
-        href="https://www.openpeeps.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      All character assets taken from{" "}
+      <a href="https://www.openpeeps.com/" target="_blank" rel="noopener noreferrer" className={styles.link}>
         Open Peeps
       </a>
       , by Pablo Stanley
